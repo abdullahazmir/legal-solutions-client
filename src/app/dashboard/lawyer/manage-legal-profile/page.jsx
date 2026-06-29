6ea80ffc02db0b9b5ff18e689c863e91decab920
abdullahazmir <ab.azmir@gmail.com>
2026-06-29 20:03:01 +0600

    dep

--- src/app/dashboard/lawyer/manage-legal-profile/page.jsx
+++ src/app/dashboard/lawyer/manage-legal-profile/page.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
+            <h1>legal profiles...</h1>
             <DashboardStats></DashboardStats>
         </div>
     );
