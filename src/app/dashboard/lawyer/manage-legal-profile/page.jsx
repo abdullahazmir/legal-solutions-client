9a0e56a95690a4c839dd0033b5a8346be7dc550f
abdullahazmir <ab.azmir@gmail.com>
2026-06-29 20:00:30 +0600

    deploy

---
+++ src/app/dashboard/lawyer/manage-legal-profile/page.jsx
@@ -0,0 +1,12 @@
+import { DashboardStats } from '@/components/dashboard/DashboardStats';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <DashboardStats></DashboardStats>
+        </div>
+    );
+};
+
+export default page;
